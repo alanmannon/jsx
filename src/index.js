558908ebc0784b0aef3ed0882b74053d3caf15bc
Alan <alanmannon@gmail.com>
2020-10-28 19:21:36 -0500

    created basic page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom';
 
 
 // Create a react component
+const App = function () {
+  return <div>Hi there</div>
+}
 
+// or const App = () => {}
 
 // Take the react component and show on browser
+ReactDOM.render(
+  <App />, document.querySelector('#root')
+);
