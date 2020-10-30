17f472be18a54fbb457de113e414721a5da26fc0
Alan <alanmannon@gmail.com>
2020-10-30 18:38:47 -0500

    finished with basic jsx info

--- src/index.js
+++ src/index.js
@@ -5,10 +5,25 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = function () {
-  return <div>Hi there</div>
+  const buttonText = 'Click Here';
+  return (
+    <div>
+      <label htmlFor="name" className="label" >Enter name:</label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+    </div >
+  );
 }
+// TIPS
+
 
 // or const App = () => {}
+// jsx props should use double quotes, non-jsx use single quotes
+// use className instead of class tag to avoid compile confusion
+
+
+
+
 
 // Take the react component and show on browser
 ReactDOM.render(
